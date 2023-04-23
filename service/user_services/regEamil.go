485f654767eb0c383ed1a81822f016ee8ac4cde6
S0tephen <S0tephen@outlook.com>
2023-04-23 12:31:09 +0800

    将 email.go 中的 发件信息 通过读取配置文件 运行

--- service/user_services/regEamil.go
+++ service/user_services/regEamil.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 	"index_Demo/dao/redisServer"
+	"index_Demo/gen/orm/dal"
 	"index_Demo/gen/response"
 	"index_Demo/utils/email"
 	"net/http"
@@ -19,6 +20,12 @@ func RegEmailCode(ctx *gin.Context) {
 	err := ctx.BindJSON(&regEmail)
 	if err != nil {
 		ctx.JSON(http.StatusUnprocessableEntity, response.New("系统出错联系管理员", err))
+		return
+	}
+	u := dal.User
+	user, _ := u.WithContext(ctx).Where(u.Uemail.Eq(regEmail.Email)).First()
+	if user != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, response.New("邮箱已存在", nil))
 		return
 	}
 	code, emailErr := email.SendMail(regEmail.Email)
--- utils/email/email.go
+++ utils/email/email.go
@@ -3,6 +3,7 @@ package email
 import (
 	"crypto/tls"
 	"github.com/jordan-wright/email"
+	"github.com/spf13/viper"
 	"math/rand"
 	"net/smtp"
 )
@@ -10,17 +11,20 @@ import (
 func SendMail(ToEmail string) (*string, error) {
 	e := email.NewEmail()
 
-	mailUserName := "w2477284535@163.com" //邮箱账号
-	mailPassword := "FBBUWAVMRDHYSNZC"    //邮箱授权码
-	code := RandCode()                    //发送的验证码
-	Subject := "验证码发送测试"                  //发送的主题
+	mailUserName := viper.GetString("emailCode.seedEmail")    //邮箱账号
+	mailPassword := viper.GetString("emailCode.mailPassword") //邮箱授权码
+	code := RandCode()                                        //发送的验证码
+	Subject := viper.GetString("emailCode.seedSubject")       //发送的主题
+	addr := viper.GetString("emailCode.addr")
+	host := viper.GetString("emailCode.host")
+	serverName := viper.GetString("emailCode.Server")
 
-	e.From = "B盘 <w2477284535@163.com>"
+	e.From = viper.GetString("emailCode.eFrom")
 	e.To = []string{ToEmail}
 	e.Subject = Subject
 	e.HTML = []byte("<p>你好！</p>你的验证码为：<h1>" + code + "</h1>" + "<p>***该验证码5分钟内有效***</p>")
-	err := e.SendWithTLS("smtp.163.com:465", smtp.PlainAuth("", mailUserName, mailPassword, "smtp.163.com"),
-		&tls.Config{InsecureSkipVerify: true, ServerName: "smtp.163.com"})
+	err := e.SendWithTLS(addr, smtp.PlainAuth("", mailUserName, mailPassword, host),
+		&tls.Config{InsecureSkipVerify: true, ServerName: serverName})
 	if err != nil {
 		return nil, err
 	}
