0e9b89017c80551f488c40fde8386033a660aabd
S0tephen <S0tephen@outlook.com>
2023-04-23 22:30:43 +0800

    新增 管理员 查看(分页处理) 现有用户数量

--- service/admin_services/rootUser.go
+++ service/admin_services/rootUser.go
@@ -2,20 +2,58 @@ package admin_services
 
 import (
 	"github.com/gin-gonic/gin"
+	mysql "index_Demo/dao/mysql"
 	"index_Demo/gen/orm/dal"
+	"index_Demo/gen/orm/model"
 	"index_Demo/gen/response"
 	"index_Demo/utils/middleware/auth"
 	"net/http"
 )
 
+type Pagination struct {
+	Total    int64 `json:"total"`
+	PageNum  int   `json:"pageNum"`
+	PageSize int   `json:"pageSize"`
+}
+
 func User(ctx *gin.Context) {
 	user := auth.CurrentUser(ctx)
+	var userSql model.User
 	u := dal.User
+	db := mysql.DB.GetDb()
 
-	//查询数据库该用户对应的信息
+	//查询数据库该用户是否为管理员
 	userInfo, _ := u.WithContext(ctx).Where(u.UID.Eq(user.UID)).First()
-	if userInfo.UID == 0 {
-		ctx.JSON(http.StatusUnprocessableEntity, response.New("用户不存在", nil))
+	if userInfo.IsAdmin == false {
+		ctx.JSON(http.StatusUnprocessableEntity, response.New("没有权限", nil))
 		return
 	}
+
+	pagination := Pagination{}
+	err := ctx.BindJSON(&pagination)
+	if err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, response.New("绑定数据失败", err))
+		return
+	}
+	if pagination.PageNum == 0 {
+		pagination.PageNum = 1
+	}
+	switch {
+	case pagination.PageSize > 100:
+		pagination.PageSize = 100
+	case pagination.PageSize <= 0:
+		pagination.PageSize = 10
+	}
+	offsetVal := (pagination.PageNum - 1) * pagination.PageSize
+	db.Model(userSql).Count(&pagination.Total).Limit(pagination.PageSize).Offset(offsetVal).Order("create_at desc").Find(&userSql)
+	ctx.JSON(200, gin.H{
+		"msg":  "查询成功",
+		"code": 200,
+		"data": gin.H{
+			"list":     userSql,
+			"total":    pagination.Total,
+			"pageNum":  pagination.PageNum,
+			"pageSize": pagination.PageSize,
+		},
+	})
 }
